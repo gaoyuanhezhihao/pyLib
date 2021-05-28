ef9a8d27bcc9cc8e82f79966556e9be7777a8b9b
zhihaohe <zhihaohe@autox.ai>
2021-05-28 19:56:43 +0800

    images_player: print current index of sample. use PlayerUI

--- OneLib/ImageViewer/images_player.py
+++ OneLib/ImageViewer/images_player.py
@@ -6,6 +6,7 @@ import argparse
 import cv2
 from time import time,sleep
 import threading
+from player_ui import PlayerUI
 
 
 def load_image(fp, dst, key, cond_var=None):
@@ -36,11 +37,13 @@ class ImageMap:
     def inc(self, step = 1) :
         self.i_ += step
         self.i_ = min(self.i_, len(self.fnames_)-1)
+        print(self.i_)
         self.update_jobs()
 
     def dec(self, step = 1):
         self.i_ -= step
         self.i_ = max(self.i_, 0)
+        print(self.i_)
         self.update_jobs()
 
 
@@ -79,45 +82,6 @@ if __name__ == '__main__':
     fnames = os.listdir(args.directory)
     fnames = sorted(fnames, key=lambda x:int(x[:x.find('.')]))
 
-    freq = 10
-    stop = False
-    i = 0
     imgs = ImageMap(args.directory)
-
-    while i < len(fnames):
-        start = time()
-        # f = fnames[i]
-        # img = cv2.imread(os.path.join(args.directory, f))
-        cv2.imshow("a", imgs.get_image())
-        # cmd = cv2.waitKey(1000/speed)
-        e = 1000 * (time() - start)
-        wait_time = 1000/freq - e
-        if wait_time < 1:
-            print("freq is too high")
-            wait_time = 1
-        cmd = cv2.waitKey(int(wait_time))
-        # print(type(cmd))
-        # print(cmd)
-        # exit()
-        if cmd == 83: # -> next frame
-            stop = True
-            imgs.inc()
-            # i += 1
-        elif cmd == 81: # <-- prev frame
-            stop = True
-            imgs.dec()
-            # i -= 1
-        elif cmd == 97: # a: slow down
-            freq -= 2
-            freq = max(1, freq)
-            print("freq=", freq)
-        elif cmd == 100: # d: speed up
-            freq += 2
-            print("freq=", freq)
-        elif cmd == 32: #space: toggle pause/play
-            stop = not stop
-        elif cmd == 113: # q: quit
-            exit(0)
-        if not stop:
-            imgs.inc()
-            # i += 1
+    ui = PlayerUI(imgs)
+    ui.run()
