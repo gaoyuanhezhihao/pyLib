82c88284a1df7bf26b439a07885a71ddebfca9b0
zhihaohe <zhihaohe@autox.ai>
2022-03-30 10:42:27 +0800

    +list directory by pattern option

--- OneLib/ImageViewer/images_player.py
+++ OneLib/ImageViewer/images_player.py
@@ -1,5 +1,4 @@
 import sys
-sys.path = ['/home/zhihaohe/.local/lib/python3.5/site-packages/'] + sys.path
 import os
 from os.path import join
 import sys
@@ -10,6 +9,8 @@ import threading
 from player_ui import PlayerUI
 from thread_pool import ThreadPool
 from functools import partial
+import glob
+import re
 
 
 def load_image(fp, dst, key, cond_var=None):
@@ -29,7 +30,7 @@ def isfloat(value):
     return False
 
 def list_first_file_of_subdirectories(directory):
-    TARGET_FILE = "point_association_1.bmp"
+    TARGET_FILE = "point_association_1.png"
     file_paths = []
     for name in os.listdir(directory):
         if os.path.isdir(join(directory, name)) and isfloat(name):
@@ -39,15 +40,32 @@ def list_first_file_of_subdirectories(directory):
                 assert os.path.exists(fp)
     return sorted(file_paths, key=lambda x:float(x.split('/')[-2]))
 
-def list_float_timed_files(directory):
+def list_float_timed_files(directory, extension):
     fnames = os.listdir(directory)
-    fnames = [f for f in fnames if f.endswith('.bmp')]
+    fnames = [f for f in fnames if f.endswith(extension)]
     fnames = sorted(fnames, key=lambda x:float(x[:x.rfind('.')]))
     return [join(directory, fname) for fname in fnames]
+
+def list_timed_files(pattern):
+    files = glob.glob(pattern)
+    p = re.compile(r'\d*\.\d*')
+    items = [float(re.search(p, f).group()) for f in files if re.search(p, f)]
+    sorted_items = sorted(items)
+    return [f[1] for f in sorted_items]
+
+def list_images(directory, extension):
+    fnames = os.listdir(directory)
+    fnames = [f for f in fnames if f.endswith(extension)]
+    fnames = sorted(fnames)
+    return [join(directory, fname) for fname in fnames]
+
 
 class ImageMap:
 
     def __init__(self, file_paths):
+        if len(file_paths) == 0:
+            print('Error: no files loaded')
+            exit(-1)
         self.prefetch_number = 8
         self.map_ = {}
         self.thread_map_ = {}
@@ -57,16 +75,16 @@ class ImageMap:
         self.thread_pool = ThreadPool(self.prefetch_number)
         self.update_jobs()
 
-    def inc(self, step = 1) :
+    def inc(self, step=1):
         self.i_ += step
         self.i_ = min(self.i_, len(self.file_paths)-1)
-        print(self.file_paths[self.i_])
+        print('inc:', self.file_paths[self.i_])
         self.update_jobs()
 
-    def dec(self, step = 1):
+    def dec(self, step=1):
         self.i_ -= step
         self.i_ = max(self.i_, 0)
-        print(self.file_paths[self.i_])
+        print('dec:', self.file_paths[self.i_])
         self.update_jobs()
 
 
@@ -101,7 +119,8 @@ class ImageMap:
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
-    p.add_argument('directory')
+    p.add_argument('--pattern')
+    p.add_argument('--directory')
     p.add_argument('--extension', dest='extension', default='jpg', help='extension of image files')
     args = p.parse_args()
 
@@ -109,7 +128,13 @@ if __name__ == '__main__':
     # fnames = os.listdir(args.directory)
     # fnames = sorted(fnames, key=lambda x:int(x[:x.find('.')]))
 
-    image_fetcher = ImageMap(list_float_timed_files(args.directory))
+    image_files = []
+    if args.directory:
+        image_files = list_images(args.directory, args.extension)
+    elif args.pattern:
+        print("use glob pattern")
+        image_files = glob.glob(args.pattern)
+    image_fetcher = ImageMap(image_files)
     ui = PlayerUI(image_fetcher)
     ui.run()
     image_fetcher.stop()
