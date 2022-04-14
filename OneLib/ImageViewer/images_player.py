7897d337de38d1d51df8258ebf1628cd78908417
zhihaohe <zhihaohe@autox.ai>
2022-04-14 09:53:05 +0800

    cache image file lists

--- OneLib/ImageViewer/images_player.py
+++ OneLib/ImageViewer/images_player.py
@@ -11,6 +11,7 @@ from thread_pool import ThreadPool
 from functools import partial
 import glob
 import re
+# import ipdb
 
 
 def load_image(fp, dst, key, color_scale):
@@ -55,11 +56,34 @@ def list_timed_files(pattern):
     sorted_items = sorted(items)
     return [f[1] for f in sorted_items]
 
-def list_images(directory, extension):
-    fnames = os.listdir(directory)
-    fnames = [f for f in fnames if f.endswith(extension)]
-    fnames = sorted(fnames)
-    return [join(directory, fname) for fname in fnames]
+def list_images(args):
+    file_paths = []
+    if args.load_cached_file_list and load_cached_file_list(args.directory+args.extension, file_paths):
+        print('load cached files')
+    else:
+        fnames = os.listdir(args.directory)
+        fnames = [f for f in fnames if f.endswith(args.extension)]
+        fnames = sorted(fnames)
+        file_paths = [join(args, directory, fname) for fname in fnames]
+
+    if args.cache_file_list:
+        cache_file_list(args.directory + args.extension, file_paths)
+    return file_paths
+
+
+def glob_images(args):
+    file_paths = []
+    file_list_id = args.pattern.replace('*', '_star_')
+    file_list_id = file_list_id.replace('/', '_')
+    if args.load_cached_file_list and load_cached_file_list(file_list_id, file_paths):
+        print('load cached files')
+    else:
+        file_paths = glob.glob(args.pattern)
+        if args.cache_file_list:
+            cache_file_list(file_list_id, file_paths)
+    return file_paths
+
+
 
 
 class ImageMap:
@@ -68,13 +92,14 @@ class ImageMap:
         if len(file_paths) == 0:
             print('Error: no files loaded')
             exit(-1)
-        self.prefetch_number = 8
+        self.thread_number = 16
+        self.prefetch_number = 128
         self.map_ = {}
         self.thread_map_ = {}
         self.i_ = 0
         self.buffered_ = set()
-        self.file_paths= file_paths
-        self.thread_pool = ThreadPool(self.prefetch_number)
+        self.file_paths = file_paths
+        self.thread_pool = ThreadPool(self.thread_number)
         self.color_scale = color_scale
         self.update_jobs()
 
@@ -96,7 +121,7 @@ class ImageMap:
             if i not in self.map_:
                 self.buffered_.add(i)
                 self.map_[i] = i
-                self.thread_pool.add_job(partial(load_image, self.file_paths[i], self.map_, i))
+                self.thread_pool.add_job(partial(load_image, self.file_paths[i], self.map_, i, self.color_scale))
 
         # remove too old image
         old_ids = []
@@ -119,11 +144,37 @@ class ImageMap:
         self.thread_pool.stop()
 
 
+def cache_file_list(file_list_id, file_lists):
+    print('cache_file_list')
+    CACHE_DIR = './cached_file_lists/'
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    with open(join(CACHE_DIR, file_list_id+'.txt'), 'w') as f:
+        print(f.name)
+        f.write('\n'.join(file_lists))
+
+
+def load_cached_file_list(file_list_id, image_files):
+    CACHE_DIR = './cached_file_lists/'
+    cache_file_path = join(CACHE_DIR, file_list_id+'.txt')
+    if os.path.exists(cache_file_path):
+        print('use cahced file lists')
+        with open(cache_file_path, 'r') as f:
+            for l in f.readlines():
+                image_files.append(l)
+        return True
+    else:
+        print('no cache records:', cache_file_path)
+        return False
+
+
+
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--pattern')
     p.add_argument('--directory')
+    p.add_argument('--cache_file_list', type=bool, default=False, help='For files on NFS, it cost much time to list directory or glob, cache them and reuse next time could speed up the loading period')
+    p.add_argument('--load_cached_file_list', type=bool, default=True)
     p.add_argument('--extension', dest='extension', default='jpg', help='extension of image files')
     p.add_argument('--color_scale', type=int, default=None, help='color scale will be multiplied to every r,g,b of every pixel')
     args = p.parse_args()
@@ -134,11 +185,10 @@ if __name__ == '__main__':
 
     image_files = []
     if args.directory:
-        image_files = list_images(args.directory, args.extension)
+        image_files = list_images(args)
     elif args.pattern:
-        print("use glob pattern")
+        image_files = glob_images(args)
     image_fetcher = ImageMap(image_files, args.color_scale)
-    image_fetcher = ImageMap(image_files)
     ui = PlayerUI(image_fetcher)
     ui.run()
     image_fetcher.stop()
